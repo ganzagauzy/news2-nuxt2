9ec8a8d5a6358694b3ca5ead73f34656f8357a5f
Ganza Ntambuko <ganzantambuko@gmail.com>
2021-09-30 14:16:41 +0200

    news

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
 
 
     <div class="container">
-      <h1>Knews To Know About Us</h1>
+      <h1>News To Know </h1>
       <lgcarddisplay
         v-for="cardinfo in lgcardinfo.slice(0,3)"
         :key="cardinfo.id"
